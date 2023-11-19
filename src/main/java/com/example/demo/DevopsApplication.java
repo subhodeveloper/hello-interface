3f4558da67873c384827444022fbc9348f06017d
AG <test@domain.com>
2023-11-19 08:49:49 +0530

    Adding a new mapping for hi

--- src/main/java/com/example/demo/DevopsApplication.java
+++ src/main/java/com/example/demo/DevopsApplication.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
+import java.time.Month;
 import java.time.Year;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +20,12 @@ public class DevopsApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		String data = "Hello " + name +" , this is " + Year.now().getValue();
+		return data;
+	}
+
+	@GetMapping("/hi")
+	public String hi(@RequestParam(value = "name", defaultValue = "World") String name) {
+		String data = "Hi " + name + " , this is a test message";
 		return data;
 	}
 
